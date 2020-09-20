e106cc2926814e86bedbb9cbd17c9e7c22cea44b
lnnhpmp <xufangyu709@gmail.com>
2020-09-20 10:27:01 +0200

    setup

---
+++ cat-wiki-challenge/src/components/catwikiTitle.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="title-container">
+            <div>
+                <h1 class="CatWiki-title">Catwiki</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Mystery+Quest&display=swap');
+
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.title-container {
+    display: flex;
+    flex-direction: column;
+
+    height: 80vh;
+    width: 90vw;
+    background: url("../../images/HeroImagelg.png");
+    border-radius: 42px 42px 0px 0px;
+}
+
+.CatWiki-title {
+    width: 207px;
+    height: 87px;
+    margin: 25px 50px 75px 100px;
+
+    font-family: Mystery Quest;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 64px;
+    line-height: 87px;  
+
+    color: #FFFFFF;
+}
+</style>
---
+++ cat-wiki-challenge/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <catwikiTitle></catwikiTitle>
+</template>
+
+<script>
+import catwikiTitle from '../components/catwikiTitle'
+export default {
+    components: {
+        catwikiTitle
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
